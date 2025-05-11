27b6e54eca8a284e3f8d87872ba5091981a44731
Abdullah-Jawed <abdullahjawed.diligenttek@gmail.com>
2025-05-11 17:59:57 +0500

    some isses fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -134,7 +134,7 @@ const Home = () => {
       <TestimonialsSec />
 
       <section className='security relative z-[3] py-[8rem] mb-[5rem]'>
-        <video src={SecurityVideo} autoPlay muted playsInline loop className='opacity-85 absolute top-0 left-0 w-full h-full object-cover z-[-1] object-center'></video>
+        <video src={SecurityVideo} autoPlay muted playsInline preload='metadata' loop className='opacity-85 absolute top-0 left-0 w-full h-full object-cover z-[-1] object-center'></video>
         <div className="container">
           <div className='w-full'>
             <img src={LockImg} className='mx-auto mb-6' alt="image" />
